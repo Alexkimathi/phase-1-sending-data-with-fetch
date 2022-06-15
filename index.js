0fbdaa0b31d3be07f6bbddc4d00c40ccd5b93de8
alex kimathi <keemalekie@gmail.com>
2022-06-15 14:41:20 +0300

    commit tree 25565380c36de6604d0cea06af1b004adba419e7

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+
+const submitData = (name,email)=>{
+    const users={name: name, email:email}
+
+    return fetch("http://localhost:3000/users",{
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept:"application/json",
+        },
+        body: JSON.stringify(users)
+    })
+    .then(res => res.json())
+    .then(data =>{
+        document.body.innerHTML=data["id"]
+    })
+    .catch(err =>{
+        document.body.innerHTML=err.message
+        alert("name or email is incorrect");
+        
+    })
+
+}
+
+
